e4d75349460fd6fb6d8ada51eab3e514f440d5eb
kentsin <kentsin@yahoo.com>
2020-05-01 22:04:49 +0800

    h

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
+import "flag"
+
 var (
 	singleThread  bool   = false
 	filenameMatch string = "*"
 )
 
 func main() {
-
+	flag.stringVar(&filenameMatch, "name", "*", "filename pattern")
+	flag.BoolVar(&singleThread, "single thread", false, "use single thread only")
 }
